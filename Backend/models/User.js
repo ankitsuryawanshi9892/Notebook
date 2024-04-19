b3bed729547af49717b645f852cfe0e97a2dcef8
Ankit Suryawanshi <126219523+ankitsuryawanshi9892@users.noreply.github.com>
2024-04-19 10:09:53 +0530

    Model: Change default value of avatar

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -22,7 +22,7 @@ const UserSchema = new Schema({
     },
     avatar: {
         type: String,
-        default: '/images/avatar.jpg' // Default image path
+        default: '/images/user.png' // Default image path
     },
     date: {
         type: Date,
